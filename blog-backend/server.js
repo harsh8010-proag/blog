8a3c2201d1deb8188fb1ec3214e5644d3f615f2f
harsh8010-proag <harishwavare@gmail.com>
2026-06-03 12:21:01 +0530

    fix cors origin

--- blog-backend/server.js
+++ blog-backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://tsblog.onrender.com"],
 
     credentials: true
 
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 
 app.use(cookieParser());
 app.use('/users', userRoute);
-app.use('/blogs', blogRouter)
+app.use('/blogs', blogRouter);
 app.use('/contacts', contactRoute);
 
 app.listen(process.env.PORT, () => {
